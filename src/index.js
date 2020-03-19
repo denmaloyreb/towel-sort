359472396c7bc30be76098ef17b393f3a294c63b
DenisRebenock <denmaloyreb@mail.ru>
2020-03-19 16:54:41 +0300

    refactor: add code in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let resultArray = new Array();
+  for (let i=0; i<matrix.length; i++) {
+    if (i!=0 && i%2!=0) {
+      matrix[i].reverse();
+    }
+    for (let j=0; j<matrix[i].length; j++) {
+      resultArray.push(matrix[i,j]);
+    }
+  }
+  return resultArray;
 }
