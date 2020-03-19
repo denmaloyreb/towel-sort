3b1c96483e8e0f70e753a66984125cf36068bc5d
DenisRebenock <denmaloyreb@mail.ru>
2020-03-19 17:37:52 +0300

    fix: fixed bugs

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@
 
 module.exports = function towelSort (matrix) {
   let resultArray = new Array();
+  if (matrix.length==0) {
+    return (matrix);
+  }
+  else if (!matrix) {
+    resultArray = [];
+    return resultArray;
+  }
+  else {
   for (let i=0; i<matrix.length; i++) {
     if (i!=0 && i%2!=0) {
       matrix[i].reverse();
     }
     for (let j=0; j<matrix[i].length; j++) {
-      resultArray.push(matrix[i,j]);
+      resultArray.push(matrix[i][j]);
     }
   }
-  return resultArray;
+  
+   return resultArray;
+}
 }
